12ca1a7388994a0e60ae34be15cdb930551cc54f
kallaugher <alicekallaugher@gmail.com>
2016-07-27 14:04:39 -0400

    create school class with methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+      @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, student_names|
+      @roster[grade] = student_names.sort
+    end
+  end
+
+end
